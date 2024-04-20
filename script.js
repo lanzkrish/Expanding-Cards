d991716e03898aab0472ef52811152ef8c13783e
Dhananjay Sahoo <kanha94377@gmail.com>
2024-04-20 17:41:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function expand(){
+
+}
+
+const panels =document.querySelectorAll('.panel');
+
+console.log(panels);
+
+panels.forEach((panel)=>{
+    console.log(panel)
+    panel.addEventListener('click',()=>{
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+function removeActiveClasses(){
+    panels.forEach((panel)=>{
+        panel.classList.remove('active')
+    })
+}
